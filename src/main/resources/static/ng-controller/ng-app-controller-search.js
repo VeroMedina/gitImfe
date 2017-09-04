137f149c1dd1bfacbeb6dd0401c61d7f2dfb27a6
Lenovo <Lenovo@LENOVO-T420>
2017-09-04 14:03:44 +0200

    cambios del dia 04/09/17

--- src/main/resources/static/ng-controller/ng-app-controller-search.js
+++ src/main/resources/static/ng-controller/ng-app-controller-search.js
@@ -1,13 +1,24 @@
 app.controller('ng-app-controller-search', 
 ['$scope', '$http','$timeout', function ($scope,$http,$timeout)
-  {
-        (function() //funcion para que se muestre todos los dulces sin buscar nada
+  {     
+        /**
+         * Json para obtener la id contador y definir uso de clase notfound
+         * @type type
+         */
+        var items=
+                {
+                    elemento: document.querySelector ('#contador'),
+                    style:
+                            {
+                                classNotFound: 'notFound'
+                            }
+                };
+                
+                
+        //funcion para que se muestre todos los dulces sin buscar nada
+        (function() 
         {
-            $http.post('/searchAll', //nombre del controlador
-            {
-
-
-            })
+            $http.post('/searchAll',{}) //nombre del controlador
             .then(function (response)
             {
                 var data=response.data;
@@ -15,16 +26,17 @@ app.controller('ng-app-controller-search',
                 $scope.bbdd=data;
                 
                 //Envio la cuenta de los items
-                var cont=response.data.length;
+                var cont=data.length;
                 $scope.contador=cont;
 
-            })       
+            });       
 
         })();
-
+        
+        //funcion para mostrar dulces segun busqueda
        var timer=null;
 
-       $scope.search=function(e) //funcion para mostrar dulces segun busqueda
+       $scope.search=function(e) 
        {
            var val=e.target.value;
 
@@ -39,17 +51,39 @@ app.controller('ng-app-controller-search',
                   })
                   .then(function (response)
                   {
-                      var data=response.data;
-                      $scope.bbdd=data;
+                      var data = response.data;
+                      var cont = data.length;   //Envio la cuenta de los items
                       
-                       //Envio la cuenta de los items
-                      var cont=response.data.length;
-                      $scope.contador=cont;
+                      $scope.bbdd     = data;
+                      $scope.contador = cont;
+                      
+                      console.log("antes"+cont);
+                      
+                      
+                      
+                      if (cont === 0)
+                      {
+                         items.elemento.classList.add(items.style.classNotFound);
+                           console.log("dentro if"+cont); 
+                      }
+                      else if(items.elemento.classList.contains(items.style.classNotFound))
+                      {
+                         items.elemento.classList.remove(items.style.classNotFound);
+                      }  
+                    
+                        
+                         console.log("fuera if"+cont); console.log(items.elemento);
+                         
+                    
                       
 
                   });
             },2000);
         };
+        
+       
+        
+        
   }]);
   
 
